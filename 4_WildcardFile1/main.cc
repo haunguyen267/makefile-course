5c1a3e9590fb239fa82af4efdfa1d87a1e107dbf
Hau Nguyen <haunguyen26797@gmail.com>
2024-07-25 17:34:36 +0700

    First commit

---
+++ 4_WildcardFile1/main.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "lib.h"
+using namespace std;
+
+int main() {
+  say_hello();
+
+  return 0;
+}
---
+++ 8_Additional/src/lib.cc
@@ -0,0 +1,6 @@
+#include <iostream>
+using namespace std;
+
+void say_hello() {
+  cout << "Hello World from Lib" << endl;
+}
